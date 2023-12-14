6bddd498a59a7b8e138f6631f43bc45e9e645da9
MarkusMaal2 <markus.maal@voco.ee>
2023-12-14 13:07:40 +0200

    Connect to MongoDB and create article

--- index.ts
+++ index.ts
@@ -1,9 +1,26 @@
 import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
+
+import mongoose from "mongoose"
+import articleController from "./controllers/articleController";
+
+mongoose.connect("mongodb+srv://markusmaal:A9bpQ9RbUSTbZ34o@cluster0.u5k9lk4.mongodb.net/?retryWrites=true&w=majority");
+const database = mongoose.connection;
+database.on('error', (error) => {
+    console.log(error);
+})
+
+database.once('connected', () => {
+    console.log("Database Connected")
+})
 
 const app: Express = express();
-app.get('/', (req:Request, res:Response) =>{
+app.use(bodyParser.json());
+// create application/json parser
+app.use('/', articleController)
+/*app.get('/', (req:Request, res:Response) =>{
     res.send("Express + TypeScript Server");
-})
+})*/
 
 app.listen(3000, () => {
     console.log(`[server]: Server is running at http://localhost:3000`)
